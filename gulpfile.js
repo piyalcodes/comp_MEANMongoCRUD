904fc07bf11e11bab01ebb707ac2c672a2c8e69b
piyalcodes <tenamax@gmail.com>
2018-02-03 10:35:12 +0530

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass');  
+
 
 
 gulp.task('sass', function() { 
-	return gulp.src('./sass/*.scss')
+	return gulp.src(['!./sass/variables.scss', './sass/*.scss'])
 	.pipe(sass())
-	.pipe(gulp.dest('./css'))
-})
+	.pipe(gulp.dest('./public/css'))
+});
 
 gulp.task('watch', function() { 
-	gulp.watch('./sass/*.scss', ['sass'])
-})
+	gulp.watch('./sass/*.scss', ['sass']);
+});
 
-gulp.task('default', ['sass', 'watch'])
+gulp.task('default', ['sass' , 'watch'])
